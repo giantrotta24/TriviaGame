444671d2fc8e54dd1e2ea4e2d83a41455e963372
giantrotta24 <giantrotta@me.com>
2018-11-21 18:41:46 -0500

    display 30 second timer boii

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,43 @@
 $(document).ready(function () {
 
-    $(".button").click(function() {
-        $("#timer").text("Hello World!");
+    var questionArray;
+    var answerArray;
+
+    function countDown() {
+        var count = 30;
+
+        var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+        function timer() {
+            count--;
+            if (count <= 0) {
+                clearInterval(counter);
+                console.log("success")
+                //counter ended, do something here
+
+                startGame();
+            }
+
+            
+            document.getElementById("timer").innerHTML = "Time Remaining: " + count;
+        };
+    }
+
+
+    $(".button").click(function () {
         $(".button").hide();
+        startGame();
     })
 
-    
+    function startGame() {
+        countDown();
+    }
 
 
-   
+
+
+
+
+
 });
 
