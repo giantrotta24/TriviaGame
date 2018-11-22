380aaca11c14f29a32f73f79fd8ead435308a8aa
giantrotta24 <giantrotta@me.com>
2018-11-22 15:28:16 -0500

    transition from questions but timer error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,42 @@
 $(document).ready(function () {
-
+    //questions
     var questionArray = ["What QB led the Browns to three AFC championship games during the 1980's?", "Which Browns player was voted to the Pro Bowl every year that he played?"];
+    //answers
     var answerArray = [["Otto Graham", "Vinny Testaverde", "Brian Sipe", "Bernie Kosar"], ["Otto Graham", "Joe Thomas", "Ozzie Newsome", "Jim Brown"]];
+    //correct answers
     var correctAnswer = ["Bernie Kosar", "Jim Brown"];
+
+    var correctAnswerInfo = ["You're correct! <br> <br> Quarterback Bernie Kosar, who the Browns drafted in 1985, led the team to three AFC Championship games in the late 1980s but lost each time to the Denver Broncos.", "You're correct! <br> <br> Brown is the only player in NFL history to average over 100 rushing yards per game for his career."]
+    
     var questionCounter = 0;
+
+    var count = 30;
+
+
+    //questions HTML
+    var gameHTML = $(".questionBox");
 
     //start game button click function
     $(".button").click(function () {
+        //hide button
         $(".button").hide();
+        //call start game function
         startGame();
+        countDown();
     });
 
     //answer click function
-    $(".answer").click(function() {
+    $("body").on("click", ".answer", function (event) {
+
         selectedAnswer = $(this).text();
+
+        //correct answer
         if (selectedAnswer === correctAnswer[questionCounter]) {
             // alert("correct!")
-            correctAnswer();
+            correctChoice();
 
         }
+        //incorrect answer
         else {
             alert("wrong!")
         }
@@ -26,29 +44,62 @@ $(document).ready(function () {
 
     //30 second countdown function
     function countDown() {
-        var count = 30;
-
-        var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+        clearInterval(timer);
+        setInterval(timer, 1000); //1000 will  run it every 1 second
 
         function timer() {
             count--;
             if (count <= 0) {
-                clearInterval(counter);
+                clearInterval(timer);
                 //counter ended, do something here
-                
+
             }
 
-            
+
             document.getElementById("timer").innerHTML = "" + count;
         };
+    }
+
+    //correct answer function
+    function correctChoice() {
+        // alert("good job homie");
+        
+
+        //hide questions
+        $(gameHTML).hide();
+
+        //display answer box
+        $(".answerBox").show();
+
+        //display message
+        $("#message").html(correctAnswerInfo[questionCounter]);
+
+        //call wait function
+        setTimeout(wait, 4000);
+    }
+
+    //wait function
+    function wait() {
+        if (questionCounter < 1) {
+            questionCounter++;
+            $(".answerBox").hide();
+
+            clearInterval(timer);
+            
+            countDown();
+            
+            startGame();
+            
+        } else {
+            alert("Game Over!");
+        }
     }
 
     //start game function
     function startGame() {
         //display game
-        $(".questionBox").show();
-        //start countdown
-        countDown();
+        $(gameHTML).show();
+
         //display question
         $("#statement").html(questionArray[questionCounter]);
         //display answers
@@ -58,10 +109,6 @@ $(document).ready(function () {
         $("#answerFour").html(answerArray[questionCounter][3]);
     }
 
-    //correct answer function
-    function correctAnswer() {
-        
-    }
 
 
 
