b11ef1d03257b1eb417fe44699761e6cb4fce9e6
giantrotta24 <giantrotta@me.com>
2018-11-25 18:20:19 -0500

    image display correct and incorrect

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,13 +5,17 @@ $(document).ready(function () {
     var answerArray = [["Otto Graham", "Vinny Testaverde", "Brian Sipe", "Bernie Kosar"], ["Otto Graham", "Joe Thomas", "Ozzie Newsome", "Jim Brown"], ["Kardiac Kids", "Comeback Kids", "Orange Crush", "Orange Wave"], ["Ozzie Newsome", "Webster Slaughter", "Earnest Byner", "Bernie Kosar"], ["Jim Brown", "Otto Graham", "Marion Motley", "Paul Brown"], ["Otto Graham", "Bernie Kosar", "Brady Quinn", "Mike Phipps"], ["5", "7", "14", "0"], ["10", "5", "13", "7"]];
     //correct answers
     var correctAnswer = ["Bernie Kosar", "Jim Brown", "Kardiac Kids", "Earnest Byner", "Otto Graham", "Bernie Kosar", "0", "13"];
-
+    //correct answer fun facts
     var correctAnswerInfo = ["You're correct! <br> <br> Quarterback Bernie Kosar, who the Browns drafted in 1985, led the team to three AFC Championship games in the late 1980s but lost each time to the Denver Broncos.", "You're correct! <br> <br> Brown is the only player in NFL history to average over 100 rushing yards per game for his career.", "You're correct! <br> <br> Due to their penchant for having games decided in the final moments, the team was nicknamed the Kardiac Kids.", "You're correct! <br> <br>Just before crossing the goal line, Byner was hit by cornerback Jeremiah Castille, who stripped the ball and then fell on the fumble to preserve the win and assure Denver of its second straight AFC title and a trip to Super Bowl XXII.", "You're correct! <br> <br> Having won seven championships in 10 seasons and reached the championship game in every year he played, Graham is regarded by sportswriters as one of the greatest winners of all time.", "You're correct! <br> <br> In 1990 and 1991, Kosar set a league record by throwing 308 consecutive passes without an interception, which stood for almost two decades.", "You're correct! <br> <br> After Art Modell moved the team to Baltimore, there were three years of suspension before the Browns started play again in 1999 under new owner Al Lerner.", "You're correct! <br> <br> From 2012 to 2018, the Cleveland Browns collected 13 first-round draft picks."];
+    //incorrect answer display
+    var incorrectAnswerInfo = ["Sorry, you're wrong! <br> <br> The correct answer is Bernie Kosar, who the Browns drafted in 1985.", "Sorry, you're wrong! <br> <br> The correct answer is Jim Brown, who is the only player in NFL history to average over 100 rushing yards per game for his career.", "Sorry, you're wrong! <br> <br> Due to their penchant for having games decided in the final moments, the team was nicknamed the Kardiac Kids.", "Sorry, you're wrong! <br> <br>It was Earnest Byner who was hit by cornerback Jeremiah Castille, who stripped the ball and then fell on the fumble to preserve the win for Denver.", "Sorry, you're wrong! <br> <br> The correct answer is Otto Graham, who won seven championships in 10 seasons and reached the championship game in every year he played.", "Sorry, you're wrong! <br> <br> In 1990 and 1991, it was Bernie Kosar that set a league record by throwing 308 consecutive passes without an interception, which stood for almost two decades.", "Sorry, you're wrong! <br> <br> After Art Modell moved the team to Baltimore, there were three years of suspension before the Browns started play again in 1999 under new owner Al Lerner.", "Sorry, you're wrong! <br> <br> From 2012 to 2018, the Cleveland Browns collected 13 first-round draft picks."];
+    //correct image display
+    var correctImage = ["<img class='gif' src='https://media.giphy.com/media/3ohhwv7ECyvHRqDWkU/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/y1S51KbB8BcKQ/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/3LKsNtFkdU8A0aNk51/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/4HtLaI9ZW9mL2mN2kW/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/3FQolxq6b1kuXjmL0o/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/j9UtP3FpyTGth736UR/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/3oxHQsn4iKGF1WZrYQ/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/RN96WHKThqODSLu64b/giphy.gif' height='200px' width='400px'>"]
+    //incorrect image display
+    var incorrectImage = ["<img class='gif' src='https://media.giphy.com/media/4GZZHFSIudeyXQRPqo/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/3ohhwsgvH7cdLq1dfO/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/1wpN7qx1onStZGZqsS/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/2A0M2S6sqaLGJsOnDv/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/l0MYsr8vxTRlb8CZi/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/l3vR6rraIKItRVtM4/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/5aWCuDb2DMFb3pK8LB/giphy.gif' height='200px' width='400px'>", "<img class='gif' src='https://media.giphy.com/media/dQpbz3zOa6pFG0K6dP/giphy.gif' height='200px' width='400px'>"]
 
-    var incorrectAnswerInfo = ["Sorry, you're wrong! <br> <br> Quarterback Bernie Kosar, who the Browns drafted in 1985, led the team to three AFC Championship games in the late 1980s but lost each time to the Denver Broncos.", "Sorry, you're wrong! <br> <br> Brown is the only player in NFL history to average over 100 rushing yards per game for his career.", "Sorry, you're wrong! <br> <br> Due to their penchant for having games decided in the final moments, the team was nicknamed the Kardiac Kids.", "Sorry, you're wrong! <br> <br>Just before crossing the goal line, Byner was hit by cornerback Jeremiah Castille, who stripped the ball and then fell on the fumble to preserve the win and assure Denver of its second straight AFC title and a trip to Super Bowl XXII.", "Sorry, you're wrong! <br> <br> Having won seven championships in 10 seasons and reached the championship game in every year he played, Graham is regarded by sportswriters as one of the greatest winners of all time.", "Sorry, you're wrong! <br> <br> In 1990 and 1991, Kosar set a league record by throwing 308 consecutive passes without an interception, which stood for almost two decades.", "Sorry, you're wrong! <br> <br> After Art Modell moved the team to Baltimore, there were three years of suspension before the Browns started play again in 1999 under new owner Al Lerner.", "Sorry, you're wrong! <br> <br> From 2012 to 2018, the Cleveland Browns collected 13 first-round draft picks."];
-    
     var questionCounter = 0;
-
+    //timer count
     var count = 30;
 
 
@@ -66,7 +70,7 @@ $(document).ready(function () {
     //correct answer function
     function correctChoice() {
         // alert("good job homie");
-        
+
 
         //hide questions
         $(gameHTML).hide();
@@ -76,6 +80,9 @@ $(document).ready(function () {
 
         //display message
         $("#message").html(correctAnswerInfo[questionCounter]);
+
+        //display image
+        $("#image").html(correctImage[questionCounter]);
 
         //call wait function
         setTimeout(wait, 7000);
@@ -106,11 +113,11 @@ $(document).ready(function () {
             $(".answerBox").hide();
 
             clearInterval(timer);
-            
+
             countDown();
-            
+
             startGame();
-            
+
         } else {
             alert("Game Over!");
         }
